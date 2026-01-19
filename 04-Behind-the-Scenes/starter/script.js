bdf21892998fc6f14f1fa5c8e75521e183dc277c
BrijK <Brijk0625@gmail.com>
2026-01-19 16:23:33 +0530

    scope-of-variables

--- 04-Behind-the-Scenes/starter/script.js
+++ 04-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,14 @@
-'use strict';
+// 'use strict';
+
+function greet() {
+  //function scope
+  let gr = 'hello';
+  return gr;
+}
+// console.log(gr); // checking gr variable is accessible or not, not accessible
+// Block scope
+if (true) {
+  let a = 9;
+  console.log('hello world !');
+}
+// console.log(a); not accessible here
