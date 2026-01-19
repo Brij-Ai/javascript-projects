05ef13631f773c8cccb32cf6826449341cb3e9a8
BrijK <Brijk0625@gmail.com>
2026-01-19 19:18:58 +0530

    Finished-Variable-Function-Hoisting

--- 04-Behind-the-Scenes/starter/script.js
+++ 04-Behind-the-Scenes/starter/script.js
@@ -45,3 +45,43 @@
 // calAge(2004);
 
 // Hoisting Practice
+// Variable Hoisting
+console.log(mySelf); // undefined
+// console.log(year);
+// console.log(job);
+
+var mySelf = 'Brij';
+// TDZ
+let job = 'Unemployed';
+const year = '2026';
+
+// Function Hoisting
+console.log(addDec(8, 4));
+// console.log(addExr(8, 4));
+// console.log(addArrow(8, 4));
+
+function addDec(a, b) {
+  return a + b;
+}
+// TDZ
+const addExr = function (a, b) {
+  return a + b;
+};
+// TDZ
+const addArrow = (a, b) => a + b;
+
+// Example
+console.log(cartItem); // undefined
+if (!cartItem) deleteCarItem(); // true bcz cartItme = undefined
+var cartItem = 10;
+function deleteCarItem() {
+  console.log('Cart is deleted!');
+}
+
+// let const and var
+var x = 1;
+let y = 2;
+const z = 3;
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
