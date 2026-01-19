e5b5897032ae00f507273c82f828785d43a5e4a9
BrijK <Brijk0625@gmail.com>
2026-01-19 18:49:41 +0530

    starting-Hoistion-Practice

--- 04-Behind-the-Scenes/starter/script.js
+++ 04-Behind-the-Scenes/starter/script.js
@@ -13,33 +13,35 @@
 // }
 // console.log(a); not accessible here
 // Global Function
-function calAge(birthYear) {
-  console.log(firstName); // firstName = vinace consoled
-  // Name is not deifined
-  //   console.log(name); // Not as any variable declared
-  const age = 2025 - birthYear;
+// function calAge(birthYear) {
+//   console.log(firstName); // firstName = vinace consoled
+// Name is not deifined
+//   console.log(name); // Not as any variable declared
+//   const age = 2025 - birthYear;
 
-  function displayAge() {
-    console.log(`${firstName} age is ${age} and born in ${birthYear}`);
-    let output = 'OLD O/P';
-    if (birthYear >= 1981 && birthYear <= 1996) {
-      var millenial = true;
-      let str = `Oh, You are a millenial , ${firstName}`;
-      console.log(str);
-      // Reassighning
-      output = 'NEW O/P';
-      // Function are also block scoped
-      function add(a, b) {
-        return a + b;
-      }
-    }
-    // console.log(str);
-    // console.log(millenial); // undefined
-    // add(3, 4); blocked scoped
-    console.log(output);
-  }
-  displayAge();
-  return age;
-}
-const firstName = 'Vinace';
-calAge(2004);
+//   function displayAge() {
+//     console.log(`${firstName} age is ${age} and born in ${birthYear}`);
+//     let output = 'OLD O/P';
+// if (birthYear >= 1981 && birthYear <= 1996) {
+//   var millenial = true;
+//   let str = `Oh, You are a millenial , ${firstName}`;
+//   console.log(str);
+//   // Reassighning
+//   output = 'NEW O/P';
+//   // Function are also block scoped
+//   function add(a, b) {
+//     return a + b;
+//   }
+// }
+// console.log(str);
+// console.log(millenial); // undefined
+// add(3, 4); blocked scoped
+//     console.log(output);
+//   }
+//   displayAge();
+//   return age;
+// }
+// const firstName = 'Vinace';
+// calAge(2004);
+
+// Hoisting Practice
