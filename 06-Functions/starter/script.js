6ae022bd48ed346d405a87383171b68f0c8a5d03
BrijK <Brijk0625@gmail.com>
2026-01-26 18:56:20 +0530

    finished-defualt-perameter

--- 06-Functions/starter/script.js
+++ 06-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+// Started function again
+
+//Default perameter in function
+const bookings = [];
+const createBooking = function (
+  flightNum = 'Air india',
+  numPassenger = 1,
+  price = 1000,
+) {
+  const booking = {
+    flightNum,
+    numPassenger,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('BH001', 9, 3);
+createBooking('BH001', undefined, 3); // default value
+createBooking('BH002'); // default value
+createBooking(); // all default value
