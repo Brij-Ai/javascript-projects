39fa56d9a933218b5b6fb1230a397f4d965e0aa7
BrijK <Brijk0625@gmail.com>
2026-01-28 18:17:28 +0530

    End !!!

--- 06-Functions/starter/script.js
+++ 06-Functions/starter/script.js
@@ -2,21 +2,43 @@
 // Started function again
 
 //Default perameter in function
-const bookings = [];
-const createBooking = function (
-  flightNum = 'Air india',
-  numPassenger = 1,
-  price = 1000,
-) {
-  const booking = {
-    flightNum,
-    numPassenger,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+// const bookings = [];
+// const createBooking = function (
+//   flightNum = 'Air india',
+//   numPassenger = 1,
+//   price = 1000,
+// ) {
+//   const booking = {
+//     flightNum,
+//     numPassenger,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+// createBooking('BH001', 9, 3);
+// createBooking('BH001', undefined, 3); // default value
+// createBooking('BH002'); // default value
+// createBooking(); // all default value
+
+//pass by value and refrence
+
+const flight = 'LHBG76';
+const user = {
+  name: 'Jexa hummern',
+  passport: 8948384,
 };
-createBooking('BH001', 9, 3);
-createBooking('BH001', undefined, 3); // default value
-createBooking('BH002'); // default value
-createBooking(); // all default value
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'BN876';
+  passenger.name = 'Mr' + passenger.name;
+  if (passenger.passport === user.passport) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passpost');
+  }
+};
+
+checkIn(flight, user);
+checkIn(user);
+checkIn(flight);
