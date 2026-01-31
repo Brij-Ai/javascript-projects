503f96231f847b7d9f9d779fb202fdf409976365
BrijK <Brijk0625@gmail.com>
2026-01-31 16:04:38 +0530

    High_Ordered_Function

--- 06-Functions/starter/script.js
+++ 06-Functions/starter/script.js
@@ -23,22 +23,53 @@
 
 //pass by value and refrence
 
-const flight = 'LHBG76';
-const user = {
-  name: 'Jexa hummern',
-  passport: 8948384,
+// const flight = 'LHBG76';
+// const user = {
+//   name: 'Jexa hummern',
+//   passport: 8948384,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'BN876';
+//   passenger.name = 'Mr' + passenger.name;
+//   if (passenger.passport === user.passport) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passpost');
+//   }
+// };
+
+// checkIn(flight, user);
+// checkIn(user);
+// checkIn(flight);
+
+//Starting high order function and first class function
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+// console.log(oneWord('hello you not ready for this !'));
+
+const toUpperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join();
 };
 
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'BN876';
-  passenger.name = 'Mr' + passenger.name;
-  if (passenger.passport === user.passport) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passpost');
-  }
+// High order function
+const transform = function (str, fn) {
+  console.log(`Original string : ${str}`);
+  console.log(`Transformed string : ${fn(str)}`);
+  console.log(`Transformed by : ${fn.name}`);
 };
 
-checkIn(flight, user);
-checkIn(user);
-checkIn(flight);
+transform('What is the going on !', toUpperFirstWord);
+transform('What is the going on !', oneWord);
+
+// function
+const highFive = function () {
+  console.log('👋👋');
+};
+
+document.body.addEventListener('click', highFive);
+
+['Sunny', 'Nirmal'].forEach(highFive);
