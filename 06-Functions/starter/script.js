782a87875a34fa032033a4510d88372722e0e90e
BrijK <Brijk0625@gmail.com>
2026-02-05 18:22:21 +0530

    ---__----__00

--- 06-Functions/starter/script.js
+++ 06-Functions/starter/script.js
@@ -105,33 +105,46 @@
 
 // Challenge 1#
 
-const poll = {
-  question: 'What is your favourite programming language ?',
-  options: ['0:JavaScript', '1: Python', '2: Rust', '3: C++'],
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    const answer = Number(
-      prompt(
-        `${this.question}\n${this.options.join('\n')}\n(Write option number)`,
-      ),
-    );
-    typeof answer === 'number' &&
-      answer < this.answers.length &&
-      this.answers[answer]++;
-    this.displayResults();
-    this.displayResults('string');
-  },
-  displayResults(type = 'array') {
-    if (type === 'array') {
-      console.log(this.answers);
-    } else if (type == 'string') {
-      console.log(`Poll results are ${this.answers.join(', ')}`);
-    }
-  },
+// const poll = {
+//   question: 'What is your favourite programming language ?',
+//   options: ['0:JavaScript', '1: Python', '2: Rust', '3: C++'],
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')}\n(Write option number)`,
+//       ),
+//     );
+//     typeof answer === 'number' &&
+//       answer < this.answers.length &&
+//       this.answers[answer]++;
+//     this.displayResults();
+//     this.displayResults('string');
+//   },
+//   displayResults(type = 'array') {
+//     if (type === 'array') {
+//       console.log(this.answers);
+//     } else if (type == 'string') {
+//       console.log(`Poll results are ${this.answers.join(', ')}`);
+//     }
+//   },
+// };
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+
+// IIFE
+
+const runOnce = function () {
+  console.log('He never will run again !');
 };
+runOnce();
 
-document
-  .querySelector('.poll')
-  .addEventListener('click', poll.registerNewAnswer.bind(poll));
-
-poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+// call itself
+// IIFE
+(function () {
+  console.log('He never will run again !');
+})();
